419cd74f6796d0f68b507c4c0d101cf769709c92
Kresna Sucandra <kresnasucandra@gmail.com>
2023-05-14 23:50:39 +0800

    CYBNET-75 add create connection fn

--- pallets/edge-connect/src/lib.rs
+++ pallets/edge-connect/src/lib.rs
@@ -1,3 +1,23 @@
+//! Pallet Edge Connect
+//! 
+//! - [`Config`]
+//! - [`Call`]
+//! - [`Pallet`]
+//! - [`Error`]
+//! - [`Event`]
+//! - [`Storage`]
+//! 
+//! ## Overview
+//! 
+//! Edge Connect is a pallet that allows users to create and remove connections between Cyborg blockchain and external edge servers.
+//! 
+//! ## Interface
+//! 
+//! ### Dispatchable Functions
+//! 
+//! * `create_connection` - Creates a connection between Cyborg blockchain and an external edge server.
+//! * `remove_connection` - Removes a connection between Cyborg blockchain and an external edge server.
+
 #![cfg_attr(not(feature = "std"), no_std)]
 
 pub use pallet::*;
@@ -28,25 +48,28 @@ pub mod pallet {
 
 	// The pallet's runtime storage items.
 	#[pallet::storage]
-	#[pallet::getter(fn something)]
-	pub type Something<T> = StorageValue<_, u32>;
+	#[pallet::getter(fn connection)]
+	pub type Connection<T> = StorageValue<_, u32>; // TODO: change to the proper data structure
 
 	// Pallets use events to inform users when important changes are made.
 	#[pallet::event]
 	#[pallet::generate_deposit(pub(super) fn deposit_event)]
 	pub enum Event<T: Config> {
 		/// Event documentation should end with an array that provides descriptive names for event
-		/// parameters. [something, who]
-		SomethingStored { something: u32, who: T::AccountId },
+		/// parameters. [connection, who]
+		ConnectionCreated { connection: u32, who: T::AccountId },
+		/// Event documentation should end with an array that provides descriptive names for event
+		/// parameters. [connection, who]
+		ConnectionRemoved { connection: u32, who: T::AccountId },
 	}
 
 	// Errors inform users that something went wrong.
 	#[pallet::error]
 	pub enum Error<T> {
-		/// Error names should be descriptive.
-		NoneValue,
-		/// Errors should have helpful documentation associated with them.
-		StorageOverflow,
+		/// Returned if the connection already exists.
+        ConnectionAlreadyExists,
+        /// Returned if the connection does not exist.
+        ConnectionDoesNotExist,
 	}
 
 	// Dispatchable functions allows users to interact with the pallet and invoke state changes.
@@ -58,20 +81,23 @@ pub mod pallet {
 		/// storage and emits an event. This function must be dispatched by a signed extrinsic.
 		#[pallet::call_index(0)]
 		#[pallet::weight(10_000 + T::DbWeight::get().writes(1).ref_time())]
-		pub fn do_something(origin: OriginFor<T>, something: u32) -> DispatchResult {
+		pub fn create_connection(origin: OriginFor<T>, connection: u32) -> DispatchResult {
 			// Check that the extrinsic was signed and get the signer.
 			let who = ensure_signed(origin)?;
 
+			// Check that the connection does not already exist.
+			ensure!(!<Connection<T>>::exists(), Error::<T>::ConnectionAlreadyExists);
+
 			// Update storage.
-			<Something<T>>::put(something);
+			<Connection<T>>::put(connection);
 
 			// Emit an event.
-			Self::deposit_event(Event::SomethingStored { something, who });
+			Self::deposit_event(Event::ConnectionCreated { connection, who });
 
 			// Return a successful DispatchResult
 			Ok(())
 		}
 
-		
+		// TODO: create remove_connection fn
 	}
 }
